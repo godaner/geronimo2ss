6212e483e9da4b50b0b01e62a1e75b1f1bb74067
zhangke <ke.zhang@han-networks.com>
2020-08-13 16:54:24 +0800

    rm bin , enc def is nil

--- c/cfg/config.go
+++ c/cfg/config.go
@@ -16,7 +16,7 @@ func ParseFlag() {
 	overbose := flag.Bool("overbose", false, "overBose")
 	logpath := flag.String("logpath", "", "log path")
 	loglev := flag.Int("loglev", 0, "log lev , option is : 0:CRIT,1:ERRO,2:WARN,3:NOTI,4:INFO,5:DEBU")
-	enc := flag.String("enc", "aes-256-cfb@123qwe", "encryption : method@password , method option is : aes-128-cfb,aes-192-cfb,aes-256-cfb,aes-128-ctr,aes-192-ctr,aes-256-ctr,des-cfb,bf-cfb,cast5-cfb,rc4-md5,rc4-md5-6,chacha20,chacha20-ietf,salsa20")
+	enc := flag.String("enc", "", "encryption : method@password , method option is : aes-128-cfb,aes-192-cfb,aes-256-cfb,aes-128-ctr,aes-192-ctr,aes-256-ctr,des-cfb,bf-cfb,cast5-cfb,rc4-md5,rc4-md5-6,chacha20,chacha20-ietf,salsa20")
 	flag.Parse()
 
 	LocalAddr = *localaddr
